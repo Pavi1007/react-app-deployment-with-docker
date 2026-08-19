a5a9accc249e96f323dbd36973305e3bf7dd62da
root <root@ip-172-31-38-49.ec2.internal>
2026-08-19 11:38:06 +0000

    Updated Docker Hub repository

--- build.sh
+++ build.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+set -e
 
-#login into DockerHub:
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
+# Login to Docker Hub
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-#stopping existing container:
-docker stop react
-docker rm react
+# Stop and remove old container if it exists
+docker stop react || true
+docker rm react || true
 
-#building a image:
+# Build Docker image
 docker build -t react-ci/cd .
 
-#running a container from the created image:
-docker run -d -it --name react -p 80:80 react-ci/cd
+# Run container
+docker run -d --name react -p 80:80 react-ci/cd
 
-#pushing the image to dockerhub:
-docker tag react-ci/cd naveen712/react-app:ci-cd
-docker push naveen712/react-app:ci-cd
+# Tag image
+docker tag react-ci/cd pavipalsamy7/react-app:ci-cd
+
+# Push image
+docker push pavipalsamy7/react-app:ci-cd
